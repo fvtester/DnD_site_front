9014368421de55d7a0b8c49d49e3f7585c02758d
MityushinaYV <Mitu-yana@mail.ru>
2023-09-07 20:57:40 +0300

    initial commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { MessageService } from './message.service'
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+  const heroes = of(HEROES);
+  this.messageService.add('HeroService: fetched heroes');
+  return heroes;
+}
+  constructor(private messageService: MessageService) { }
+}
