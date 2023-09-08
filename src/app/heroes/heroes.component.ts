3db73841e803ce1b754d59e07a6f2283d6021b18
MityushinaYV <Mitu-yana@mail.ru>
2023-09-08 15:57:01 +0300

    some styles were added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Hero} from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,14 +12,17 @@ export class HeroesComponent {
 selectedHero?: Hero;
 onSelect(hero: Hero): void {
   this.selectedHero = hero;
+  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
 }
 heroes: Hero[] = [];
 
-constructor(private heroService: HeroService) {}
+constructor(private heroService: HeroService, private messageService: MessageService) {}
 
 ngOnInit(): void {
   this.getHeroes();
+
 }
+
 
 getHeroes(): void {
    this.heroService.getHeroes()
